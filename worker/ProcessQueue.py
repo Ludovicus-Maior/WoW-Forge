aacff4893fc7984d5dc7aec458a57745d810b17e
Ludovicus <LuisOrtiz@Verizon.NET>
2014-04-25 16:28:59 -0400

    Correct wait_time_seconds to max limit, add progress printing and fix 15 minute exit for ProcessQueue.py

--- worker/ProcessQueue.py
+++ worker/ProcessQueue.py
@@ -4,6 +4,7 @@ import boto.sqs
 import json
 import os
 import subprocess
+import sys
 import time
 import traceback
 
@@ -25,9 +26,11 @@ def Connect(region="us-west-2", queue="WorkerQ"):
 
 def GetJsonMessage(q):
     while True:
-        rs = q.get_messages(num_messages=1,  wait_time_seconds=60*5)
+        rs = q.get_messages(num_messages=1,  wait_time_seconds=20)
         if len(rs) > 0:
             break
+	else:
+	    return None
     msg = rs[0].get_body()
     q.delete_message(msg)
     jmsg = json.loads(msg)
@@ -50,10 +53,16 @@ if __name__ == "__main__":
     stime = time.time()
     q = Connect(region=os.environ["WF_SQS_REGION"], queue=os.environ["WF_SQS_QUEUE"])
     m = GetJsonMessage(q)
-    while m:
-        DoMessage(q, m)
+    while True:
+	if m:
+            DoMessage(q, m)
+	else:
+	    sys.stdout.write(",")
+	    sys.stdout.flush()
 	ctime = time.time()
 	# After 15 minutes, allow the driver to do maintainance
 	if ( (ctime-stime) > 60*15 ):
+	    sys.stdout.write(".")
+	    sys.stdout.flush()
 	    exit(0)
         m = GetJsonMessage(q)
