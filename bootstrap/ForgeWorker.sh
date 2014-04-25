162aee7948468ccf9b3edd3b1b1342dc19eb2594
Luis Ortiz <LuisOrtiz@Verizon.NET>
2014-04-25 18:54:57 +0100

    Convert ForgeWorker.sh to use no ruby.  Trim install list.

--- bootstrap/ForgeWorker.sh
+++ bootstrap/ForgeWorker.sh
@@ -8,7 +8,7 @@ exec 2>&1
 
 # Update APT and install necessary packages
 apt-get update
-apt-get install -y debconf-utils python-pip python-dev ruby1.9.1 ruby1.9.1-dev build-essential wget libruby1.9.1 libxslt-dev libxml2-dev mysql-client libmysqlclient-dev git
+apt-get install -y debconf-utils python-pip python-dev build-essential wget mysql-client libmysqlclient-dev git
 
 # Lets use the new commands!
 hash -r
@@ -25,34 +25,31 @@ export REGION=$(wget -qO- http://169.254.169.254/latest/meta-data/placement/avai
 pip install boto --upgrade
 pip install mysql-python
 
-# Install all required GEMs
-gem install aws-sdk --no-rdoc --no-ri
-
 # Set the HOSTNAME, comes after potential Elastic IP assignment
 export HOSTNAME=$(wget -qO- http://169.254.169.254/latest/meta-data/public-hostname)
 echo $HOSTNAME > /etc/hostname
 echo $HOSTNAME > /etc/mailname
 hostname $HOSTNAME
 
-# A ruby script to get a tarball with bootstrap information
-(
-cat <<'EOP'
-require 'aws-sdk'
-AWS.config(:credential_provider => AWS::Core::CredentialProviders::EC2Provider.new)
-AWS::S3.new.buckets["wow-forge-bootstrap"].objects["#{ARGV[0]}/#{ARGV[1]}/#{ARGV[2]}"].read { |chunk| print chunk }
-EOP
-) > /tmp/get_bootstrap_file.rb
+cat > /bin/get_bootstrap_file.py <<EOF
+from boto.s3.connection import S3Connection
+import sys
+
+conn = S3Connection()
+bucket = conn.get_bucket("wow-forge-bootstrap")
+key  = bucket.get_key("%s/%s/%s" % (sys.argv[1], sys.argv[2], sys.argv[3]) )
+key.get_contents_to_file(stdout)
+EOF
 
 # Initial config file overrides
 cd /
-ruby /tmp/get_bootstrap_file.rb $REGION worker root.tar | tar xvpo
+python /bin/get_bootstrap_file.py $REGION worker root.tar | tar xvpo
 
 # Initial worker user setup
 addgroup worker
 adduser --ingroup worker --disabled-login --gecos "Joe Worker" worker
 
-cd ~worker
-sudo -n -u worker bash -c "ruby /tmp/get_bootstrap_file.rb $REGION worker worker.tar | tar xvpo"
-sudo -n -u worker bash -c "cd /home ; git clone git@github.com:Ludovicus/WoW-Forge.git worker"
+sudo -n -u worker bash -c "cd ~worker ; python /bin/get_bootstrap_file.py $REGION worker worker.tar | tar xvpo"
+sudo -n -u worker bash -c "cd ~worker ; git init . ; git remote add -t \* -f origin git@github.com:Ludovicus/WoW-Forge.git "
 
 reboot
