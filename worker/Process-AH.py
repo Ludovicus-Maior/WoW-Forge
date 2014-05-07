df4521535c51d331abfd03a865487c5ad4493182
Ludovicus <LuisOrtiz@Verizon.NET>
2014-05-07 06:09:29 -0400

    Add the toplevel processing layer to Process-AH.py

--- worker/Process-AH.py
+++ worker/Process-AH.py
@@ -11,99 +11,58 @@ import time
 import random
 import timeout
 import sys
+import os
 import tempfile
-import rotate
 import traceback
+from urlparse import urlparse
 
 import xml.dom.minidom 
 
+# {u'rand': 0, u'auc': 110488118, u'timeLeft': u'VERY_LONG', u'bid': 455000, u'item': 30282, u'seed': 65752064, u'ownerRealm': u'Ravencrest', u'owner': u'Skaramoush', u'buyout': 470000, u'quantity': 1}
 
-#  wget http://www.wowhead.com/item=51964&xml
-# <?xml version="1.0" encoding="UTF-8"?><wowhead>
-#  <item id="51964"><name><![CDATA[Vigorous Belt]]></name><level>25</level><quality id="3">Rare</quality>
-#  <class id="4"><![CDATA[Armor]]></class><subclass id="2"><![CDATA[Leather Armor]]></subclass><icon displayId="49925">INV_Belt_03</icon>
-#  <inventorySlot id="6">Waist</inventorySlot>
-#  <randomEnchantments>
-#   <randomEnchantment id="71"><![CDATA[of the Bandit]]></randomEnchantment>
-#   <randomEnchantment id="78"><![CDATA[of the Monkey]]></randomEnchantment>
-#   <randomEnchantment id="79"><![CDATA[of the Moon]]></randomEnchantment>
-#   <randomEnchantment id="80"><![CDATA[of the Wild]]></randomEnchantment>
-#   <randomEnchantment id="82"><![CDATA[of the Vision]]></randomEnchantment>
-#  </randomEnchantments>
-#  <htmlTooltip><![CDATA[<table><tr><td><b class="q3">Vigorous Belt</b><span style="color: #ffd100"><br />Item Level 25</span><br /><!--bo-->Binds when picked up<table width="100%"><tr><td>Waist</td><th>Leather</th></tr></table><!--rf--><span>65 Armor</span><br /><!--re--><span class="q2">&lt;Random enchantment&gt;</span><!--rs--><!--e--><!--ps--><br />Durability 40 / 40</td></tr></table><table><tr><td><!--rr--><div class="whtt-sellprice">Sell Price: <span class="moneysilver">8</span> <span class="moneycopper">46</span></div></td></tr></table>]]></htmlTooltip><json><![CDATA["armor":65,"classs":4,"displayid":49925,"id":51964,"level":25,"name":"4Vigorous Belt","slot":6,"slotbak":6,"source":[2],"sourcemore":[{"icon":"inv_misc_bag_15","n":"Satchel of Helpful Goods","q":3,"t":3,"ti":51999}],"subclass":2]]></json><jsonEquip><![CDATA["armor":65,"displayid":49925,"dura":40,"sellprice":846,"slotbak":6]]></jsonEquip><link>http://www.wowhead.com/item=51964</link></item></wowhead>
+def IsItemKnown(id):
+    return True
 
-randomEnchantItems = {}
+def RecordRandomEnchant(id,enchant_rand,enchant_seed):
+    return
 
-def LoadCache(file):
-    print "Reading Data from: "+file
-    json_file=open(file,"r")
-    data = json.load(json_file)
-    json_file.close()
-    return data
 
-def SaveCache(data,file):
-    print "Writing Data to: "+file
-    rotate.rotate(file)
-    json_file=open(file,"w+")
-    json.dump(data,json_file,indent=1,sort_keys=True)
-    json_file.close()    
-  
-@timeout.timeout(8)
-def GetToon(url):
-    return json.load(urllib.urlopen(url))
+def IsToonKnown(realm,toon):
+    return False
+
+def QueryToon(realm,toon):
+    return
     
-def ProcessToon(realm,toon):
-    global realmInfo
+def RecordSibling(realm_a,realm_b):
+    return
     
-    try:
-        url = '//us.battle.net/api/wow/character/'+realm+'/'+toon
-        url = 'http:'+urllib.quote(url.encode('utf-8'))+'?fields=guild'
-        data = GetToon(url)
-        # check for lone rangers
-        if not ('guild' in data):
-            return
-        gname = data['guild']['name']
-        if not (gname in realmInfo['guilds']):
-            realmInfo['guilds'][gname] = 0
-    except (timeout.TimeoutError, IOError):
-        traceback.print_exc()
-        print "# Continue after ProcessToon(url=%s)" % url
-        pass
+
+def ScheduleNextAH():
+    """Out of the AH which have not been scanned within the past 24 hours,
+       select the oldest to process next and queue a query for it.
+    """
+    return
+
+def FlushCachedData():
+    """Write any remaining queries or database updates. """
+    return
     
 def ProcessAuctions(realm,faction,auctions):
-    global randomEnchantItems
-    global realmInfo
     print "#Processing %s faction" % faction
-    added = 0
-    new = 0
-    toons = 0 
     for auction in auctions:
-        if not (auction['owner'] in realmInfo['toons']):
-            ProcessToon(realm, auction['owner'])
-            realmInfo['toons'][auction['owner']] = 0
-            toons += 1
-        if auction['rand'] != 0:
-            # We have a RE item
-            id = str(auction['item'])
-            RAND = str(auction['rand'])
-            seed = str(auction['seed'] & 0xffff)
-            if not id in randomEnchantItems:
-                randomEnchantItems[id] = {}
-                randomEnchantItems[id]['seed'] = {}
-                new += 1   
-            if RAND in randomEnchantItems[id]:
-                randomEnchantItems[id][RAND] += 1
-            else:
-                randomEnchantItems[id][RAND] = 1
-                added +=1 
-            if seed in randomEnchantItems[id]['seed']:
-                randomEnchantItems[id]['seed'][seed] += 1
-            else:
-                randomEnchantItems[id]['seed'][seed] = 1
-#                pprint(auction)
-                added += 1
-#            print "# Item %s rand %s seed %s" % (id, RAND, seed)
-    print "# New items %d, augmented %d, toons %d" %(new, added, toons)
+        ownerRealm = auction['ownerRealm']
+        owner = auction['owner']
+        if realm != ownerRealm:
+            RecordSibling(realm, ownerRealm)
+        if not IsToonKnown(ownerRealm, owner):
+            QueryToon(ownerRealm, owner)
+        id = auction['item']
+        if not IsItemKnown(id):
+            QueryItem(id)
+        enchant_rand = auction['rand']
+        enchant_seed = auction['seed'] & 0xffff
+        if enchant_rand != 0:
+            RecordRandomEnchant(id,enchant_rand,enchant_seed)
 
 _Progress=0
 def GetAuctionDataProgress(blocks, blocks_size, file_size):
@@ -112,25 +71,36 @@ def GetAuctionDataProgress(blocks, blocks_size, file_size):
         return
     so_far = (blocks * blocks_size) / (1.0 * file_size)
     if (so_far > (_Progress+0.2)):
-        print "# Read %4.2f of the AH data" % (so_far * 100)
+        print "# Read %4.2f%% of the AH data" % (so_far * 100)
         _Progress = so_far
 
 @timeout.timeout(240)
 def GetAuctionData(url):
     global _Progress
-    tmp=tempfile.NamedTemporaryFile(suffix='.json', prefix='auctions',dir='/tmp')
-    print "# Redirected to %s, saving in %s" % (url,tmp.name)
+    parsed_url = urlparse(url)
+    guid = os.path.basename(os.path.dirname(parsed_url.path))
+    tmp_name = "/tmp/%s.json" % guid
+    if os.access(tmp_name,os.R_OK):
+        print "# Data already retrived, loading."
+        tmp = open(tmp_name, "r")
+        AH = json.load(tmp)
+        tmp.close()
+        return AH
+            
+    tmp = open(tmp_name, "w+")
+    print "# Redirected to %s, saving in %s" % (url,tmp_name)
 
     attempts = 0
     while attempts < 3:
         _Progress = 0
         try:
             info = None
-            info=urllib.urlretrieve(url, filename=tmp.name, reporthook=GetAuctionDataProgress)
+            info=urllib.urlretrieve(url, filename=tmp_name, reporthook=GetAuctionDataProgress)
         except urllib.ContentTooShortError:
             print "# Truncating file"
             tmp.truncate(0)
-        tmp.seek(0,2) # Go to EOF
+        # Go to EOF
+        tmp.seek(0,2) 
         if tmp.tell() > 1024:
             break
         attempts += 1
@@ -170,7 +140,7 @@ def ScanAuctionHouse(zone,realm):
 def ScanAuctionHouses(zone,realms=None):
     global accessAH
     
-     for realm in realms:
+    for realm in realms:
         try:
             ScanAuctionHouse(zone,realm)
             time.sleep(1.0)
@@ -193,7 +163,7 @@ def ScanAuctionHouses(zone,realms=None):
     
 try:
     if len(sys.argv) > 2:
-        ScanAuctionHouses(zone=sys.argv[1],realms=sys.argv[1:])
+        ScanAuctionHouses(zone=sys.argv[1],realms=sys.argv[2:])
     else:
         ScanAuctionHouses('US')
 except:
