e566a080179b552e999f4d73004c6094d54c6fd6
Ludovicus <LuisOrtiz@Verizon.NET>
2014-08-12 16:33:40 -0400

    Implement retry for s3 transfers

--- worker/wf/s3.py
+++ worker/wf/s3.py
@@ -4,14 +4,25 @@
 import boto
 from boto.s3.key import Key
 import os
+import time
 import wf.logger
 
-def save_ah_file(local_file, zone, realm, timestamp):
-    s3_conn = boto.connect_s3()
-    bucket = s3_conn.get_bucket('wf-ah-data')
-    key = Key(bucket)
-    key.key = "%s/%s/%s.json" % (zone, realm, timestamp)
-    wf.logger.logger.info("copying %s to %s" % (local_file, key.key))
-    key.set_contents_from_filename(local_file)
-    os.unlink(local_file)
+
+def save_ah_file(local_file, zone, realm, timestamp, retry=3):
+    try:
+        s3_conn = boto.connect_s3()
+        bucket = s3_conn.get_bucket('wf-ah-data')
+        key = Key(bucket)
+        key.key = "%s/%s/%s.json" % (zone, realm, timestamp)
+        wf.logger.logger.info("copying %s to %s" % (local_file, key.key))
+        key.set_contents_from_filename(local_file)
+    except StandardError:
+        if retry > 0:
+            wf.logger.logger.exception("Error while writing file, retry up to %d times" % retry)
+            time.sleep(1.0)
+            return save_ah_file(local_file, zone, realm, timestamp, retry=(retry-1))
+        else:
+            raise
+    finally:
+        os.unlink(local_file)
 
