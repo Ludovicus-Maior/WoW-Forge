e7283bca5fd65ea4d5ff219b5caa501b732eb49c
Luis Ortiz <LuisOrtiz@Verizon.NET>
2014-08-19 12:39:25 +0100

    Try to add maintainance detection

--- worker/ScanAuctionHouses.py
+++ worker/ScanAuctionHouses.py
@@ -21,7 +21,9 @@ def RecordTemporalPhase(ts):
 def ComputeTemporalPhase():
     return float(sum(temporal_phase))/len(temporal_phase)
 
+late_realms = 0
 def ScanAuctionHouse(zone, realm, lastScanned):
+    global late_realms
     realm_date = wf.bnet.iso_date(lastScanned)
     utc_now = datetime.datetime.utcnow()
     how_stale = (utc_now-realm_date).total_seconds()
@@ -30,10 +32,13 @@ def ScanAuctionHouse(zone, realm, lastScanned):
         return None
     if how_stale > (5*60*60):
         wf.logger.logger.error("ScanAuctionHouse(%s,%s): Realm is RANCID (%s)" % (zone, realm, lastScanned))
+        late_realms +=1
     elif how_stale > (3*60*60):
         wf.logger.logger.warning("ScanAuctionHouse(%s,%s): Realm is STALE (%s)" % (zone, realm, lastScanned))
+        late_realms +=1
     elif how_stale > (1.5*60*60):
         wf.logger.logger.info("ScanAuctionHouse(%s,%s): Realm is LATE (%s)" % (zone, realm, lastScanned))
+        late_realms +=1
     then = time.time()
     result = wf.bnet.get_auctions(zone, realm, lastScanned)
     if result:
@@ -45,16 +50,25 @@ def ScanAuctionHouse(zone, realm, lastScanned):
 
 
 def ScanAuctionHouses(zone):
-    global NO_UPDATE_NAP
+    global late_realms
     while True:
         then = time.time()
         realms = wf.rds.SelectRegionRealms(zone)
         wf.logger.logger.info("*"*80)
         wf.logger.logger.info("ScanAuctionHouses(%s) %d realms to scan" % (zone, len(realms)))
         ClearTemporalPhase()
+        late_realms = 0
         for realm in realms:
             if not ScanAuctionHouse(zone, realm, realms[realm]):
                 RecordTemporalPhase(wf.bnet.iso_date(realms[realm]))
+        if len(realms) == late_realms:
+            # If everyone is late, either it is maintenance or something is wrong.
+            # Do a slow scan until something succeeds
+            wf.logger.logger.warning("ScanAuctionHouses() %d realms LATE.  Going into slow scan mode" % late_realms)
+            for realm in realms:
+                if ScanAuctionHouse(zone, realm, realms[realm]):
+                    break
+                time.sleep(30)
         now = time.time()
         wf.logger.logger.info("ScanAuctionHouses() Scan complete in %g seconds." % (now - then))
         now = datetime.datetime.utcnow()
